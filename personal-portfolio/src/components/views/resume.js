4bc74adffd12da6d37d23a62969244710689a72b
Long <longish222@gmail.com>
2019-06-11 01:14:42 -0400

    website launched with firebase and google dns

--- personal-portfolio/src/components/views/resume.js
+++ personal-portfolio/src/components/views/resume.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 
-import ResumeFile from '../../documents/long_pham_resume_2019_pretty.pdf'
+import ResumeFile from '../../documents/long_pham_resume_2019.pdf'
 
 import { makeStyles } from '@material-ui/core/styles';
 
