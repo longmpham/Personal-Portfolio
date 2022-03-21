a708f21dada35b0c5fefe4db64f21347dc135fcb
Long <longish222@gmail.com>
2022-03-21 16:42:03 -0700

    added dynamic static games pictures

---
+++ personal-portfolio/src/images/about/games/gamesData.js
@@ -0,0 +1,38 @@
+const gamesData = [
+  {
+    "game": "Apex",
+    "url": require("./apexlegends.png"),
+  },
+  {
+    "game": "Borderlands 2",
+    "url": require("./borderlands2.png"),
+  },
+  {
+    "game": "CSGO",
+    "url": require("./counterstrike.png"),
+  },
+  {
+    "game": "Diablo",
+    "url": require("./diablo.png"),
+  },
+  {
+    "game": "League",
+    "url": require("./league.png"),
+  },
+  {
+    "game": "Genshin Impact",
+    "url": require("./genshin.png"),
+  },
+  {
+    "game": "Overwatch",
+    "url": require("./overwatch.png"),
+    "url2": require("./overwatch.mp4"),
+    "url2": require("./top500.gif"),
+  },
+  {
+    "game": "TwitchTV",
+    "url": require("./twitch.png"),
+  },
+
+]
+export default gamesData
