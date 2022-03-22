9abbf0bf663e281c853149af45789b51421d38ed
Long <longish222@gmail.com>
2022-03-21 17:50:50 -0700

    more updates on about page

--- personal-portfolio/src/components/Landing/landing.js
+++ personal-portfolio/src/components/Landing/landing.js
@@ -19,36 +19,6 @@ const Hero = () => {
         </div>
       </div>
     </>
-
-
-    // <div className="landing-container">
-    //     <Container maxWidth='sm'>
-    //         <div className="landing-title">
-    //           <Typography className="" component='h1' variant='h2' align='center' color='textPrimary' gutterBottom>
-    //             Hey There!
-    //           </Typography>
-    //         </div>
-    //         <div className="landind-description">
-    //           <Typography className="" variant="h5" align="center" color="textSecondary" paragraph>
-    //             This is my personal portfolio built with ReactJS (used in the MERN stack!). I'm always looking for improvements so please contact me with any recommendations!
-    //           </Typography>
-    //         </div>
-    //         <div className="landing-buttons">
-    //           <Grid container spacing={2} justify="center">
-    //             <Grid item>
-    //               <Button component={Link} to="/contact" variant="contained" color="primary">
-    //                 Contact Me
-    //               </Button>
-    //             </Grid>
-    //             <Grid item>
-    //               <Button component={Link} to="/about" variant="contained" color="secondary">
-    //                 About Me
-    //               </Button>
-    //             </Grid>
-    //           </Grid>
-    //         </div>
-    //     </Container>
-    // </div>
   );
 }
 
---
+++ personal-portfolio/src/images/about/adventures/adventuresData.js
@@ -0,0 +1,52 @@
+const adventuresData = [
+  {
+    "title": "1",
+    "url": require("./cant_see.jpg"),
+  },
+  {
+    "title": "2",
+    "url": require("./china_tower.jpg"),
+  },
+  {
+    "title": "3",
+    "url": require("./dog.jpg"),
+  },
+  {
+    "title": "4",
+    "url": require("./friends_in_china.jpg"),
+  },
+  {
+    "title": "5",
+    "url": require("./great_wall_of_china.jpg"),
+  },
+  {
+    "title": "6",
+    "url": require("./icy_drive.jpg"),
+  },
+  {
+    "title": "7",
+    "url": require("./new_case.jpg"),
+  },
+  {
+    "title": "8",
+    "url": require("./shanghai_tower.jpg"),
+  },
+  {
+    "title": "9",
+    "url": require("./sleepy_head.jpg"),
+  },
+  {
+    "title": "10",
+    "url": require("./smirky_dog.jpg"),
+  },
+  {
+    "title": "11",
+    "url": require("./warriors.jpg"),
+  },
+  {
+    "title": "12",
+    "url": require("./zen_garden.jpg"),
+  },
+
+]
+export default adventuresData
