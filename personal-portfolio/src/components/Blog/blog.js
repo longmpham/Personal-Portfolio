0ab2187d5f32e1226e6a3205c5b43696ca5ffd32
Long <longish222@gmail.com>
2022-03-21 23:08:30 -0700

    blog is getting better, new post and added a hide show button using React use state on arrays

--- personal-portfolio/src/components/Blog/blog.js
+++ personal-portfolio/src/components/Blog/blog.js
@@ -1,32 +1,58 @@
 import React from "react"
 
+import { FaEye, FaEyeSlash } from "react-icons/fa"
+
 import blogData from "./blogData"
 import "./blog.css"
 
 const BlogPage = () => {
 
   const [posts, setPosts] = React.useState(blogData)
-  // console.log(post)
+  const [loading, setLoading] = React.useState(false)
+  
+  // have to change blogData.js to a JSON file format for this to work.
+  // React.useEffect( () => {
+  //   const getPosts = async () => {
+  //     setLoading(true);
+  //     const response = await fetch(blogData)
+  //     console.log(response)
+  //     setPosts(response)
+  //     setLoading(false)
+  //   }
+  //   getPosts()
+  // },[])
 
+  const handleClick = (id) => {
+    setPosts(prevPosts => {
+      return prevPosts.map(post => {
+        return post.id === id ? { ...post, isShown: !post.isShown }
+        : post
+      })
+    })
+  }
 
   return (
     <>
       <div className="blog-container">
+        {/* {!loading &&  */}
         <div className="blog-main">
           {posts.map(post => {
             return (
               <div className="blog-post" key={post.id}>
                 <h1 className="blog-title">{post.title}</h1>
                 <h6 className="blog-date">{post.date}</h6>
-                {post.body.map(paragraph => {
+                <button name={post.id} className="blog-show-button" onClick={()=> {handleClick(post.id)}} value={post.isShown}>{post.isShown ? <FaEyeSlash /> : <FaEye /> }</button>
+                {!post.isShown && post.body.map((paragraph, index) => {
                   return (
-                    <p className="blog-body">{paragraph}</p>
+                      <p key={index} className="blog-body">{paragraph}</p>
                   )
-                })}
+                })
+              }
               </div>
             )
           })}
         </div>
+        {/* } */}
         {/* sidebar needs links (react useRef might be useful here...) */}
         {/* <div className="blog-sidebar">
         {posts.map(post => {
--- personal-portfolio/src/components/Blog/blogData.js
+++ personal-portfolio/src/components/Blog/blogData.js
@@ -1,21 +1,40 @@
 const postData = [
   {
     "id": "1",
-    "title": "main title 1",
-    "date": "some date 1",
-    "body": ["some lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuff", "separated by commas to indicate paragraph"],
+    "title": "Back to Scho...Coding!",
+    "date": "Jan 18, 2022",
+    "body": [
+      "Recently I've been on the rise of coding again. I haven't done this in many years and my current workplace, well, not so much coding I suppose. I've started to learn projects on the side and learn the whole web development stuff once again.", 
+      "Let's take a history check... I started out as a university student at Western Univerist, Canada. This was my upbringing as a Software Engineer and I never regret any of my decisions about this starting point. I've learned many things along the way but I never found anything that came as 'striking' or 'that's something I really really really want to do.",
+      "I've just always been interested in technology and getting my hands -- on a keyboard. I've never really been a hands on type of person but rather the thought of letting other things do the work for me. This was the highest motivation for me to think about my career and how I can keep my hands clean but still somehow produce a large amount of work.",
+      "Let's skip 7 years, I've graduated and now it's the job hunt. I found my first place working here at OSI Maritime Systems and here I actually get to do really cool things. These cool things I really cant explain but we get to work on the software Navies all across the world use! It's really cool that I get to be part of that, but these days I've been more into coding, more into automation, machine learning, artifical intelligences, anything that has some cool technology behind it. Technology improving and becoming cooler, better, faster, stronger has always been the drive for me to learn more and do more.",
+      "Okay, let's get back to web development. This is a field I've always wanted to get my hands (head) in deep and not in the dirty way! Yes, I've become a recent keyboard warrior coding away my evenings and nights learning as much as I can. Well, this blog in short is to summarize my experience as I take on this journey to envelop the web technologies and how they work and how I can use what I'm interested in to learn more and do more with techonology.",
+      "To start off, I've been getting back into React, which is what this website was created with and some other cool packages. My goal is to improve my domain knowledge here, make this website better, and eventually, make a website for my loving significant other! Till I write again, I'll give you an update on what I've learned.",
+    ],
+    "isShown": true,
   },
   {
     "id": "2",
-    "title": "main title 2",
-    "date": "some date 2",
-    "body": ["some lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuffsome lorem ipsum body stuff", "separated by commas to indicate paragraph some lorem ipsum body stuff", "separated by commas to indicate paragraph some lorem ipsum body stuff", "separated by commas to indicate paragraph some lorem ipsum body stuff", "separated by commas to indicate paragraph"],
+    "title": "They say you've only hit the tip of the iceberg...",
+    "date": "Feb 20, 2022",
+    "body": [
+      "Well, it's true. I've ventured in the world of web development at home on the side, and its honestly really really fun. I've missed coding a lot and it's really enjoyable to just sit down, think of a cool project and then code it.",
+      "Thanks to the powers of Google (which is really just every developer's best friend), I've managed to grasp the concept of how websites work from front to back. Now, just a reality check, I have no idea how the websites are really done in terms of production like Facebook, or Instagram, but I have a general idea if the userbase was about 1-10 people. I've never really dealt or thought of how crazy it can get with many more people using websites.",
+      "I've been practicing on the MERN stuff like before (MongoDB, Express, React, and NodeJS), and there is just SOOOO much to do. It is quite enjoyable but at the same time very stresful in terms of just how much there is to know and how much there is I don't know. I think my biggest fear is just not knowing enough, especially when it comes down to important periods of time like interviews, or discussing x, y, z in front of your boss.",
+      "I guess this brings it to the imposter syndrome people keep talking about for developers. Am I an imposter? Is this a self-report? From what I can tell looking at all the cool projects I've started, and mostly finished, I think I'm more of a crewmate these days than an imposter. Then when it comes to a new project, I get my hands on the keyboard again and start coding away and BAM, it hits me like an asteroid from outerspace, I don't really know what I'm truly coding. It always just feels like I've hit the tip of the iceberg...",
+    ],
+    "isShown": false,
   },
   {
     "id": "3",
     "title": "main title 3",
     "date": "some date 3",
-    "body": ["some lorem ipsum body stuff", "separated by commas to indicate paragraph some lorem ipsum body stuff", "separated by commas to indicate paragraph"],
+    "body": [
+      "some lorem ipsum body stuff", 
+      "separated by commas to indicate paragraph some lorem ipsum body stuff", 
+      "separated by commas to indicate paragraph"
+    ],
+    "isShown": false,
   },
 ]
 
